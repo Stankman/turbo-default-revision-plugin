38cb3616f20ec762d51a44f540068d55da33d1e6
Remco <habboilany@gmail.com>
2021-03-09 22:40:40 +0100

    Made tests more generic

--- TurboDefaultRevisionPlugin.Tests/Parsers/Handshake/InfoRetrieveParserTests.cs
+++ TurboDefaultRevisionPlugin.Tests/Parsers/Handshake/InfoRetrieveParserTests.cs
@@ -1,37 +1,14 @@
-﻿using AutoFixture;
-using DotNetty.Buffers;
-using Turbo.Core.Packets.Messages;
-using Turbo.Packets.Incoming;
-using Turbo.Packets.Incoming.Handshake;
+﻿using Turbo.Packets.Incoming.Handshake;
 using TurboDefaultRevisionPlugin.Parsers.Handshake;
-using Xunit;
+using TurboDefaultRevisionPlugin.Tests.Parsers;
 
 namespace Turbo.Packets.Tests.Parsers.Handshake
 {
-    public class InfoRetrieveParserTests
+    public class InfoRetrieveParserTests : EmptyPacketParserTestBase<InfoRetrieveParser, InfoRetrieveMessage>
     {
-        private readonly IFixture _fixture;
-        private readonly IParser _sut;
-
-        public InfoRetrieveParserTests()
+        public InfoRetrieveParserTests() : base()
         {
-            _fixture = new Fixture();
-            _sut = new InfoRetrieveParser();
-        }
 
-        [Fact]
-        private void Parse_WithClientPacket_ReturnsInfoRetrieveMessage()
-        {
-            // Arrange
-            var packetHeader = _fixture.Create<int>();
-            IByteBuffer buffer = Unpooled.Buffer();
-            var packet = new ClientPacket(packetHeader, buffer);
-
-            // Act
-            var result = _sut.Parse(packet);
-
-            // Assert
-            Assert.True(result is InfoRetrieveMessage);
         }
     }
 }
--- TurboDefaultRevisionPlugin.Tests/Parsers/Handshake/SSOTicketParserTests.cs
+++ TurboDefaultRevisionPlugin.Tests/Parsers/Handshake/SSOTicketParserTests.cs
@@ -1,42 +1,31 @@
 ﻿using AutoFixture;
-using DotNetty.Buffers;
-using System.Text;
-using Turbo.Core.Packets.Messages;
 using Turbo.Packets.Incoming;
 using Turbo.Packets.Incoming.Handshake;
 using TurboDefaultRevisionPlugin.Parsers.Handshake;
+using TurboDefaultRevisionPlugin.Tests.Parsers;
 using Xunit;
 
 namespace Turbo.Packets.Tests.Parsers.Handshake
 {
-    public class SSOTicketParserTests
+    public class SSOTicketParserTests : AbstractParserTestBase<SSOTicketParser, SSOTicketMessage>
     {
-        private readonly IFixture _fixture;
-        private readonly IParser _sut;
-
-        public SSOTicketParserTests()
+        public SSOTicketParserTests() : base()
         {
-            _fixture = new Fixture();
-            _sut = new SSOTicketParser();
+
         }
 
         [Fact]
         private void Parse_WithClientPacket_ReturnsSSOTicketMessage()
         {
             // Arrange
-            var packetHeader = _fixture.Create<int>();
             var ssoTicket = _fixture.Create<string>();
 
-            IByteBuffer buffer = Unpooled.Buffer();
-            var encoding = Encoding.UTF8;
+            WriteString(ssoTicket);
 
-            buffer.WriteShort(encoding.GetByteCount(ssoTicket));
-            buffer.WriteString(ssoTicket, encoding);
-
-            var packet = new ClientPacket(packetHeader, buffer);
+            var packet = new ClientPacket(_fixture.Create<int>(), _buffer);
 
             // Act
-            var result = (SSOTicketMessage)_sut.Parse(packet);
+            var result = (SSOTicketMessage) _sut.Parse(packet);
 
             // Assert
             Assert.Equal(ssoTicket, result.SSO);
