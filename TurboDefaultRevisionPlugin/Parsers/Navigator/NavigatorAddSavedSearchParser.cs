140b2fdb519545cd1c0f9ebec6ce6878abf71872
Remco <habboilany@gmail.com>
2021-03-10 00:04:42 +0100

    Added NavigatorSavedSearchParser

---
+++ TurboDefaultRevisionPlugin/Parsers/Navigator/NavigatorAddSavedSearchParser.cs
@@ -0,0 +1,15 @@
+﻿using Turbo.Core.Packets.Messages;
+using Turbo.Packets.Incoming.Navigator;
+using Turbo.Packets.Parsers;
+
+namespace TurboDefaultRevisionPlugin.Parsers.Navigator
+{
+    public class NavigatorAddSavedSearchParser : AbstractParser<NavigatorAddSavedSearchMessage>
+    {
+        public override IMessageEvent Parse(IClientPacket packet) => new NavigatorAddSavedSearchMessage
+        {
+            SearchCode = packet.PopString(),
+            Filter = packet.PopString()
+        };
+    }
+}
