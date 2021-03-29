7073dcc8f51c6215078e76ef82d55429e89e5f81
Bill <billsonnn@live.com>
2021-03-29 18:44:09 -0400

    Fix furniture serializer

--- TurboDefaultRevisionPlugin/Serializers/Rooms/Engine/Types/ObjectDataSerializer.cs
+++ TurboDefaultRevisionPlugin/Serializers/Rooms/Engine/Types/ObjectDataSerializer.cs
@@ -9,22 +9,25 @@ namespace TurboDefaultRevisionPlugin.Serializers.Rooms.Engine.Types
     {
         public static void Serialize(IServerPacket packet, IRoomObject roomObject)
         {
-            if (roomObject.Logic is IFurnitureLogic furnitureLogic)
+            if (roomObject.RoomObjectHolder is IRoomObjectFurnitureHolder furnitureHolder)
             {
-                int type = furnitureLogic.FurnitureDefinition.SpriteId;
-                packet.WriteInteger(roomObject.Id);
-                packet.WriteInteger(type);
-                packet.WriteInteger(roomObject.Location.X);
-                packet.WriteInteger(roomObject.Location.Y);
-                packet.WriteInteger((int)roomObject.Location.Rotation);
-                packet.WriteDouble(roomObject.Location.Z);
-                packet.WriteDouble(furnitureLogic.FurnitureDefinition.Z);// todo: fix this
-                packet.WriteInteger(1); // todo: extra
-                StuffDataSerializer.Serialize(packet, roomObject);
-                packet.WriteInteger(-1);//secondsToExpiration
-                packet.WriteInteger(1); // todo: fix usage policy
-                packet.WriteInteger(1); // todo: owner id
-                if (type < 0) packet.WriteString(""); // furnidata object name if it has no sprite id
+                if (roomObject.Logic is IFurnitureLogic furnitureLogic)
+                {
+                    int type = furnitureLogic.FurnitureDefinition.SpriteId;
+                    packet.WriteInteger(roomObject.Id);
+                    packet.WriteInteger(type);
+                    packet.WriteInteger(roomObject.Location.X);
+                    packet.WriteInteger(roomObject.Location.Y);
+                    packet.WriteInteger((int)roomObject.Location.Rotation);
+                    packet.WriteString(string.Format("{0:N3}", roomObject.Location.Z));
+                    packet.WriteString(string.Format("{0:N3}", furnitureLogic.FurnitureDefinition.Z));
+                    packet.WriteInteger(1); // todo: extra
+                    StuffDataSerializer.Serialize(packet, roomObject);
+                    packet.WriteInteger(-1);//secondsToExpiration
+                    packet.WriteInteger((int)furnitureLogic.FurnitureDefinition.UsagePolicy);
+                    packet.WriteInteger(furnitureHolder.PlayerId);
+                    if (type < 0) packet.WriteString(""); // furnidata object name if it has no sprite id
+                }
             }
         }
     }
